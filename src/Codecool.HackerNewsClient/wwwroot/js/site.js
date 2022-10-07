d9d07d3ed522d6b020e6647409d69e6b56d7a851
Attila Nagy <nagyattila86.hu@gmail.com>
2022-10-07 12:05:42 +0200

    Send get request to api

--- src/Codecool.HackerNewsClient/wwwroot/js/site.js
+++ src/Codecool.HackerNewsClient/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let topNewsAnchor = document.querySelector("a.nav-link.text-dark.top-news");
+topNewsAnchor.addEventListener("click", topNews);
+
+function topNews(anchor){
+    let response = apiGet("/Api/Top/")
+    console.log(response);
+}
+
+function apiGet(url) {
+    let response = fetch(url, {
+        method: "GET",
+    });
+    if (response.ok) {
+        return response.json();
+    }
+}
